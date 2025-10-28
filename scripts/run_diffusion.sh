037722ccce04204a5668510160f2d9569339a556
k-onoue <k.onoue0724@gmail.com>
2025-10-29 05:14:11 +0900

    save

---
+++ scripts/run_diffusion.sh
@@ -0,0 +1,4 @@
+python3 src/diffusion.py --params_file ./configs/diffusion_eight_gaussians.json
+python3 src/diffusion.py --params_file ./configs/diffusion_two_moons.json
+python3 src/diffusion.py --params_file ./configs/diffusion_swiss_roll.json
+hovr_experiments
